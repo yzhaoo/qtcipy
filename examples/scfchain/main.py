93d43c243fe9f788a9c2f6942f525a5cf095e406
joselado <jose.lado@aalto.fi>
2024-07-15 10:11:55 +0300

    Update

--- examples/scfchain/main.py
+++ examples/scfchain/main.py
@@ -8,5 +8,6 @@ L = 6 # exponential length
 H = hamiltonians.chain(L) # get the Hamiltonian
 
 # get the SCF object
-SCF = H.get_SCF_Hubbard(U=2.0,info=True,use_qtci=True,use_kpm=True) 
+SCF = H.get_SCF_Hubbard(U=2.0)
+SCF.solve(info=True,use_qtci=True,use_kpm=True)
 
