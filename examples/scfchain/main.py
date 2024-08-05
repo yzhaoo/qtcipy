663dec2547f279c69ebc3da7df41f519eedfc640
joselado <jose.lado@aalto.fi>
2024-08-05 07:34:14 +0300

    Update

--- examples/scfchain/main.py
+++ examples/scfchain/main.py
@@ -4,7 +4,7 @@ sys.path.append(os.getcwd()+"/../../src")
 from qtcipy.tbscftk import hamiltonians
 import numpy as np
 
-L = 4 # exponential length
+L = 8 # exponential length
 H = hamiltonians.chain(L) # get the Hamiltonian
 
 def f(r):
@@ -25,9 +25,9 @@ SCF = H.get_SCF_Hubbard(U=3.0) # generate a selfconsistent object
 
 #SCF.solve(info=True) # solve the SCF
 SCF.solve(info=True,
-        use_qtci=False,use_kpm = True,
+        use_qtci=True,use_kpm = True,
 #        info_qtci = True,
-        maxite = 2,
+        maxite = 7,
         delta= 1e-3,
         use_dynamical_qtci = False,
         backend = "C++",
